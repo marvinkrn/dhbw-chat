4a0bfd2b9a1a173fa76747ee912d83d2e79dd877
marvinkrn <kern.marvin@outlook.de>
2022-12-10 15:25:54 +0100

    Branch erstellt

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,6 +3,8 @@ import Button from '@mui/material/Button';
 import { auth } from './firebase';
 import "./Settings.css";
 import FontDownloadIcon from '@mui/icons-material/FontDownload';
+import FontSizeChanger from 'react-font-size-changer';
+
 
 function Settings() {
     return (
@@ -12,6 +14,13 @@ function Settings() {
             <h3>Erscheinungsbild</h3>
 
             <h3>Schriftgröße</h3>
+            <FontSizeChanger
+          targets={['#target .content']}
+          onChange={(element, newValue, oldValue) => {
+            console.log(element, newValue, oldValue);
+          }}                 
+        />
+                
             <div className='settings_fontSize'>
                 <FontDownloadIcon fontSize="small"/>
                 <FontDownloadIcon fontSize="medium"/>
